0667b49b2c225789d92e19607a14ae53a1b8c253
striplinsr <89102812+striplinsr@users.noreply.github.com>
2022-09-13 16:37:51 -0400

    Update deck.js

--- Game/deck.js
+++ Game/deck.js
@@ -4,12 +4,29 @@ class Deck
 	createStable()
 	{
 		const ret = [];
-
 		for(let i = 0; i < 2;i++)
 		{
-			ret[0] = Rift();
+			ret[i*13+0] = Rift();
+			ret[i*13+1] = ExoticMatter();
+			ret[i*13+2] = Deflector();
+			ret[i*13+3] = Anomaly();
+			ret[i*13+4] = Wormhole();
+			ret[i*13+5] = Rewind();
+			ret[i*13+6] = Reactor();
+			ret[i*13+7] = DarkEnergy();
+			ret[i*13+8] = FutureShift();
+			ret[i*13+9] = Singularity();
+			ret[i*13+10] = Antimatter();
+			ret[i*13+11] = TimeStrop();
+			ret[i*13+12] = Nova();
 		}
-
+		
 		return ret;
 	}
 }
+
+const cards = {
+	Rift(): 1,
+	ExoticMatter(): 2,
+	Deflector(): 3,
+	
