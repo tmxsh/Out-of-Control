9972a5c6f62ca5f4d68778308d62f64e91cb95d6
Tristan Mosher <tristan@Tristans-MacBook-Air-2.local>
2022-12-03 17:31:59 -0500

    re-adding player variables

--- Game/scripts/server.js
+++ Game/scripts/server.js
@@ -6,14 +6,31 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 //these are required for the socket io server to run
 
+var p1 = -1; //player 1
 
+var p2 = -1; //player 2
+
+//this is the page we serve
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/game.html'); //to specify which html file to connect to
   });
 
-  io.on('connection', (socket) => {
-    console.log(socket.id);
-  })
+io.on('connection', (socket) => {
+  if(p1 == -1)
+  {
+    p1 = socket.id;
+    console.log("p1 is " + p1); //checking our p1
+  }
+  else if(p2 == -1)
+  {
+    p2 = socket.id;
+    console.log("p2 is " + p2); //checking our p2
+  }
+  else
+  {
+    console.log("too many connections");
+  }
+});
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
